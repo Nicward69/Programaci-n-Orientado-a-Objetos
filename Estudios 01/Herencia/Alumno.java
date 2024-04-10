a27287ad68b31f740961a82f40a5216a850ad101
Nicward69 <ant-no.pen@live.com.mx>
2024-04-10 13:33:52 -0500

    -

---
+++ Estudios 01/Herencia/Alumno.java
@@ -0,0 +1,6 @@
+public class Alumnos extends Persona {
+    private String numControl;
+    private char status;
+
+    public Alumno (String nombre, String domiclio, Date fechaNac, String numCelular, )
+}
