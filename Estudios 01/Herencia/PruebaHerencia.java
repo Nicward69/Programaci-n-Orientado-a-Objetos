73e27fab0756d0f2d24a4a025b736359e55a592d
Nicward69 <ant-no.pen@live.com.mx>
2024-04-10 13:45:25 -0500

    -

---
+++ Estudios 01/Herencia/PruebaHerencia.java
@@ -0,0 +1,7 @@
+public class PruebaHerencia{
+    public static void main(String args[]){
+        //crear un objeto de tipo Alumno
+        Date fecha = new Date();
+        Alumno Chepe = new Alumno("Jose Rojas", "ForjaLey", "983-03-30", fecha, "23390144", 'A');
+    }
+}
