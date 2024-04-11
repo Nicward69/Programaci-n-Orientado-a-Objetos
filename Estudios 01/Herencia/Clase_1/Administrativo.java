e9ca4183b70cdc42d4ea64219aace80743142a41
Nicward69 <ant-no.pen@live.com.mx>
2024-04-11 15:40:41 +0000

    Se añadieron nuevas clases, Administrativo.java, Empleado.java, Maestro.java

---
+++ Estudios 01/Herencia/Clase_1/Administrativo.java
@@ -0,0 +1,11 @@
+public class Administrativo{
+    private byte notasBuenas;
+
+    public byte getNotasBuenas(){
+        return notasBuenas;
+    }
+
+    public void setNotasBuenas(byte notasBuenas){
+        this.notasBuenas = notasBuenas;
+    }
+}
---
+++ Estudios 01/Herencia/Clase_1/Maestro.java
@@ -0,0 +1,12 @@
+public class Maestro {
+    private byte horasGrupo;
+    
+    public byte getHorasGrupo(){
+        return horasGrupo;
+    }
+
+    public void setHorasGrupo(byte horasGrupo){
+        this.horasGrupo = horasGrupo;
+    }
+    
+}
